73e419e12b9b1784d591ed56619d481d5f074f12
Jimmy Giovanny Forero Forero <jiforero2@poligran.edu.co>
2020-04-16 16:31:07 -0500

    Especificaciones de la red

--- DevFront/src/app/app.module.ts
+++ DevFront/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RedCaracteristicasComponent } from './red-caracteristicas/red-caracteristicas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RedCaracteristicasComponent
   ],
   imports: [
     BrowserModule,
---
+++ DevFront/src/app/red-caracteristicas/red-caracteristicas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-red-caracteristicas',
+  templateUrl: './red-caracteristicas.component.html',
+  styleUrls: ['./red-caracteristicas.component.css']
+})
+export class RedCaracteristicasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
